11606c330f5768f2683d24f65d4ecfd1d46ab849
Andrew <andrewwhittington@yahoo.com>
2016-02-29 10:25:03 -0500

    Initial Commit

---
+++ CmdParser/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CmdParser
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = theDialog.FileName;
+                var list = new List<string>();
+                var fileStream = new FileStream(filename, FileMode.Open,FileAccess.Read)
+                ;
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        list.Add(line);
+                    }
+                }
+                var lines = list.ToArray();
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.WriteLine("Press any key to end.");
+            Console.ReadLine();
+        }
+    }
+}
